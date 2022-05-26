04b54cabc9c83e7801e2deff051baf8f588ec1bc
Syaifuddin <msyaifuddin1997@gmail.com>
2022-05-26 20:20:31 +0700

    initial commit updated

--- index.js
+++ index.js
@@ -6,57 +6,60 @@ const firstUser = {
   gender: "Female",
   age: 17,
   email: "monica@dingdong.com",
-  favoriteColor: new Set(
-    [
-      "Yellow",
-      "Pink",
-      "White",
-      "Purple",
-    ]
-  ),
-  isHavePet: true,
+  favoriteColor: [ ...new Set(
+      [
+        "Yellow",
+        "Pink",
+        "White",
+        "Purple",
+      ]
+    ),
+  ],
+  isHavePet: "Yes",
   education: [
     "name: SD 01, city: Jakarta, graduate: 2016",
     "name: SMP 02, city: Jakarta, graduate: 2019",
     "name: SMA 03, city: Tangerang",
   ],
-  favoriteRestaurant: new Set(
-    [
-      "Bento", 
-      "Sushi", 
-      "Pancake", 
-      "Eggy", 
-      "Tempura", 
-      "Bento", 
-      "Eggy", 
-      "Padang", 
-      "Tteok", 
-      "Sushi", 
-      "Sushi",
-    ]
-  ),
+  favoriteRestaurant: [ ...new Set(
+      [
+        "Bento", 
+        "Sushi", 
+        "Pancake", 
+        "Eggy", 
+        "Tempura", 
+        "Bento", 
+        "Eggy", 
+        "Padang", 
+        "Tteok", 
+        "Sushi", 
+        "Sushi",
+      ]
+    ),
+  ],
 };
 const secondUser = {
   name:"Wendy",
   gender: "Male",
   age: 23,
   email: "wendy@dingdong.com",
-  favoriteColor: new Set(
-    [
+  favoriteColor: [ ...new Set(
+      [
       "Blue", 
       "Black", 
       "Grey",
-    ]
-  ),
-  isHavePet: false,
+      ]
+    ),
+  ],
+  isHavePet: "No",
   education: [
     "name: SD 02, city: Jakarta, graduate: 2010",
     "name: SMP 03, city: Bogor, graduate: 2013",
     "name: SMA 01, city: Surabaya, graduate: 2016",
     "name: Universitas Maju, city: Tangerang",
   ],
-  favoriteRestaurant: new Set(
-    [
+  favoriteRestaurant: [ ...new Set(
+      [
       "Tempura",
        "Bento", 
        "Sushi", 
@@ -68,6 +71,7 @@ const secondUser = {
        "Eggy",
       ]
     ),
+  ],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
